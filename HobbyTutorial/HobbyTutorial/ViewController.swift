a09ee07391f4d046fe8acb5d05c8e0713cecef8b
altunog <altunog@gmail.com>
2022-08-23 22:14:52 +0300

    hobby and light projects

---
+++ HobbyTutorial/HobbyTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HobbyTutorial
+//
+//  Created by Oğuz Kaan Altun on 23.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
