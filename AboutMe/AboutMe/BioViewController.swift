d7597ba83af9d54f35b424c3e45023ea35c2efc1
altunog <altunog@gmail.com>
2022-09-01 23:45:29 +0300

    aboutme

---
+++ AboutMe/AboutMe/BioViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Oğuz Kaan Altun on 29.08.2022.
+//
+
+import UIKit
+
+class BioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
