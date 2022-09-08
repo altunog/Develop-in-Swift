55bb6c18ccaea060a225befbda5e11aa51bd01f4
altunog <altunog@gmail.com>
2022-09-08 12:46:46 +0300

    develop in swift

---
+++ LifeCycle/LifeCycle/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  LifeCycle
+//
+//  Created by Oğuz Kaan Altun on 6.09.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("SecondViewController - View Did Load")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("SecondViewController - View Will Appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("SecondViewController - View Did Appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("SecondViewController - View Will Disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("SecondViewController - View Did Disappear")
+    }
+    
+
+}
