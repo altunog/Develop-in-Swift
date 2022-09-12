57306eddc0ef287f9cdd027d653cf9192f4c8716
altunog <altunog@gmail.com>
2022-09-13 01:08:55 +0300

    personality quiz project

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Oğuz Kaan Altun on 11.09.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+}
---
+++ PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Oğuz Kaan Altun on 11.09.2022.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+    
+    var responses: [Answer]
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        calculatePersonalityResult()
+    }
+   
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = responses.reduce(into: [:]) { (counts, answer) in
+            counts[answer.type, default: 0] += 1
+        }
+        
+        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 > $1.1 }.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+    }
+
+}
