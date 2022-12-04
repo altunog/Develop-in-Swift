84c8207dc2a6e5b36a2d0de0cbdd99e61508ff9c
altunog <altunog@gmail.com>
2022-12-04 14:16:50 +0300

    add PersistancePractice playground

--- b - Data Collections/1 - Tables and Persistence/7 - Saving Data/PersistancePractice.playground/Contents.swift
+++ b - Data Collections/1 - Tables and Persistence/7 - Saving Data/PersistancePractice.playground/Contents.swift
@@ -10,14 +10,31 @@ let newNote = Note(title: "Grocery run",
                    text: "Pick up mayonnaise, mustard, lettuce, tomato, and pickles.",
                    timestamp: Date())
 
+let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+let archiveURL = documentDirectory.appending(path: "notes_test").appendingPathExtension("plist")
+
 let propertyListEncoder = PropertyListEncoder()
-if let encodedNote = try? propertyListEncoder.encode(newNote) {
-    print(encodedNote)
-    
-    let propertyListDecoder = PropertyListDecoder()
-    if let decodedNote = try? propertyListDecoder.decode(Note.self, from: encodedNote) {
-        print(decodedNote)
-    }
+let encodedNote = try? propertyListEncoder.encode(newNote)
+try? encodedNote?.write(to: archiveURL, options: .noFileProtection)
+
+let propertyListDecoder = PropertyListDecoder()
+if let retrievedNoteData = try? Data(contentsOf: archiveURL),
+   let decodedNote = try? propertyListDecoder.decode(Note.self, from: retrievedNoteData) {
+    print(decodedNote)
 }
 
+let note1 = Note(title: "Note One", text: "This is a sample note.", timestamp: Date())
+let note2 = Note(title: "Note Two", text: "This is another sample note.", timestamp: Date())
+let note3 = Note(title: "Note Three", text: "This is yet another sample note.", timestamp: Date())
+let notes = [note1, note2, note3]
 
+let encodedNotes = try? propertyListEncoder.encode(notes)
+try? encodedNotes?.write(to: archiveURL, options: .noFileProtection)
+
+if let retrievedNotesData = try? Data(contentsOf: archiveURL),
+   let decodedNotes = try? propertyListDecoder.decode(Array<Note>.self, from: retrievedNotesData) {
+//    print(decodedNotes)
+    decodedNotes.forEach { note in
+        print(note)
+    }
+}
