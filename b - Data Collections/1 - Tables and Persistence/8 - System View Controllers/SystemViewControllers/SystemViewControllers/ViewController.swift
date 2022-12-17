a41d8e636f636935ed3fc70958c6130a31e560bd
altunog <altunog@gmail.com>
2022-12-18 00:18:38 +0300

    create system view controller learning project

---
+++ b - Data Collections/1 - Tables and Persistence/8 - System View Controllers/SystemViewControllers/SystemViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Oğuz Kaan Altun on 17.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
