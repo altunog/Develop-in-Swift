d5c397c9792396f9e5ff1f23a5bbf766d8ac831f
altunog <altunog@gmail.com>
2023-01-26 21:00:42 +0300

    add share and safari view controllers

--- b - Data Collections/1 - Tables and Persistence/8 - System View Controllers/SystemViewControllers/SystemViewControllers/ViewController.swift
+++ b - Data Collections/1 - Tables and Persistence/8 - System View Controllers/SystemViewControllers/SystemViewControllers/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        guard let image = imageView.image else { return }
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        activityController.popoverPresentationController?.sourceView = sender
+        present(activityController, animated: true)
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        if let url = URL(string: "https://apple.com") {
+            let safariViewController = SFSafariViewController(url: url)
+            present(safariViewController, animated: true)
+        }
+    }
+    
+    
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
