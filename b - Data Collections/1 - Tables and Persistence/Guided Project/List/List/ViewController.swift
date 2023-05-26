0f6f20dd5d1be37002ac0ac16dc8144b6fc98968
altunog <altunog@gmail.com>
2023-05-26 21:52:12 +0300

    create guided project: List

---
+++ b - Data Collections/1 - Tables and Persistence/Guided Project/List/List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  List
+//
+//  Created by Oğuz Kaan Altun on 26.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
