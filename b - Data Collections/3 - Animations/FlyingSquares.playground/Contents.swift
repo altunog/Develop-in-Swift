b7cb444597eef9526a2fd7efd48fec653e44c7b9
Oğuz Kaan Altun <altunog@gmail.com>
2023-10-22 22:22:13 +0300

    use the smallFrame property

--- b - Data Collections/3 - Animations/FlyingSquares.playground/Contents.swift
+++ b - Data Collections/3 - Animations/FlyingSquares.playground/Contents.swift
@@ -20,6 +20,6 @@ UIView.animate(withDuration: 3.0) {
 } completion: { _ in
     UIView.animate(withDuration: 3.0) {
         square.backgroundColor = .systemPurple
-        square.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        square.frame = smallFrame
     }
 }
