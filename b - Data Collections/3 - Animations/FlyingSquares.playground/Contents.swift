7ee32cd40b6afcf08765d640667a5d01887f220c
Oğuz Kaan Altun <altunog@gmail.com>
2023-10-22 22:17:55 +0300

    create the Animations folder

---
+++ b - Data Collections/3 - Animations/FlyingSquares.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+import PlaygroundSupport
+
+let liveViewFrame = CGRect(x: 0, y: 0, width: 500, height: 500)
+let liveView = UIView(frame: liveViewFrame)
+liveView.backgroundColor = .white
+
+PlaygroundPage.current.liveView = liveView
+
+
+let smallFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
+let square = UIView(frame: smallFrame)
+square.backgroundColor = .systemPurple
+
+liveView.addSubview(square)
+
+UIView.animate(withDuration: 3.0) {
+    square.backgroundColor = .systemOrange
+    square.frame = CGRect(x: 150, y: 150, width: 250, height: 250)
+}
