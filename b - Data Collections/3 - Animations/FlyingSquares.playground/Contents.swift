2f4e4410c5db9c22c8fc549ae2816fbd0f5d73cd
Oğuz Kaan Altun <altunog@gmail.com>
2023-10-22 22:51:19 +0300

    change colors

--- b - Data Collections/3 - Animations/FlyingSquares.playground/Contents.swift
+++ b - Data Collections/3 - Animations/FlyingSquares.playground/Contents.swift
@@ -10,16 +10,16 @@ PlaygroundPage.current.liveView = liveView
 
 let smallFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
 let square = UIView(frame: smallFrame)
-square.backgroundColor = .systemPurple
+square.backgroundColor = .purple
 
 liveView.addSubview(square)
 
 UIView.animate(withDuration: 3.0) {
-    square.backgroundColor = .systemOrange
+    square.backgroundColor = .orange
     square.frame = CGRect(x: 150, y: 150, width: 250, height: 250)
 } completion: { _ in
     UIView.animate(withDuration: 3.0) {
-        square.backgroundColor = .systemPurple
+        square.backgroundColor = .purple
         square.frame = smallFrame
     }
 }
