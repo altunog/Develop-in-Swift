e77f62bf3a586d47c08157e38003bd6b94d204d8
Oğuz Kaan Altun <altunog@gmail.com>
2023-10-23 21:44:35 +0300

    use the spring damping animation method

--- b - Data Collections/3 - Animations/FlyingSquares.playground/Contents.swift
+++ b - Data Collections/3 - Animations/FlyingSquares.playground/Contents.swift
@@ -3,18 +3,17 @@ import PlaygroundSupport
 
 let liveViewFrame = CGRect(x: 0, y: 0, width: 500, height: 500)
 let liveView = UIView(frame: liveViewFrame)
-liveView.backgroundColor = .white
+liveView.backgroundColor = .black
 
 PlaygroundPage.current.liveView = liveView
 
 
 let smallFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
 let square = UIView(frame: smallFrame)
-square.backgroundColor = .purple
+square.backgroundColor = .systemYellow
 
 liveView.addSubview(square)
 
-UIView.animate(withDuration: 3.0, delay: 2.0, options: [.repeat], animations: {
-    square.backgroundColor = .orange
-    square.frame = CGRect(x: 150, y: 150, width: 250, height: 250)
+UIView.animate(withDuration: 1.3, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.3, options: [.repeat], animations: {
+    square.frame = CGRect(x: 150, y: 150, width: 200, height: 200)
 }, completion: nil)
