f82e94707ca5640b38511954ce4d871a9944738e
Oğuz Kaan Altun <altunog@gmail.com>
2023-10-24 22:03:52 +0300

    add iboutlets

--- b - Data Collections/3 - Animations/MusicWireframe/MusicWireframe/ViewController.swift
+++ b - Data Collections/3 - Animations/MusicWireframe/MusicWireframe/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var reverseBackground: UIView!
+    @IBOutlet weak var playPauseBackground: UIView!
+    @IBOutlet weak var forwardBackgorund: UIView!
+    @IBOutlet weak var reverseButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
