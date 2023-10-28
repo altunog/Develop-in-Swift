fb89a3555cd21844f10b04487d7161fbe42d5c9f
Oğuz Kaan Altun <altunog@gmail.com>
2023-10-28 14:37:10 +0300

    show registration screen if email address given

--- b - Data Collections/3 - Animations/lab/Contest/Contest/ViewController.swift
+++ b - Data Collections/3 - Animations/lab/Contest/Contest/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        if emailTextField.text?.isEmpty ?? true {
+            UIView.animateKeyframes(withDuration: 0.2, delay: 0, options: [.autoreverse]) {
+                self.emailTextField.transform = CGAffineTransform(translationX: 5, y: 0)
+            } completion: { _ in
+                self.emailTextField.transform = .identity
+            }
+        } else {
+            performSegue(withIdentifier: "submit", sender: self)
+        }
+    }
+    
 }
 
