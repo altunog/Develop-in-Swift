3eb3a61ca0729c25ea112f154fa3875570dd756d
Oğuz Kaan Altun <altunog@gmail.com>
2023-11-04 23:22:49 +0300

    Use the Task block to fetch data asynchronously

--- b - Data Collections/4 - WWW HTTP and URL Session/HTTPandURLSession.playground/Contents.swift
+++ b - Data Collections/4 - WWW HTTP and URL Session/HTTPandURLSession.playground/Contents.swift
@@ -4,3 +4,9 @@ let url = URL(string: "https://www.apple.com")!
 print(url.scheme as Any)
 print(url.host() as Any)
 print(url.query() as Any)
+
+Task {
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    print(data)
+}
