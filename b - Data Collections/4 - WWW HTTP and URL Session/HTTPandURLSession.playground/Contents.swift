fbb69373e0be4600c052ac167b855b0bc9a33761
Oğuz Kaan Altun <altunog@gmail.com>
2023-11-04 23:48:00 +0300

    Encode fetched data to a string

--- b - Data Collections/4 - WWW HTTP and URL Session/HTTPandURLSession.playground/Contents.swift
+++ b - Data Collections/4 - WWW HTTP and URL Session/HTTPandURLSession.playground/Contents.swift
@@ -9,7 +9,8 @@ Task {
     let (data, response) = try await URLSession.shared.data(from: url)
 
     if let httpResponse = response as? HTTPURLResponse,
-       httpResponse.statusCode == 200 {
-        print(data)
+       httpResponse.statusCode == 200,
+       let string = String(data: data, encoding: .utf8) {
+        print(string)
     }
 }
