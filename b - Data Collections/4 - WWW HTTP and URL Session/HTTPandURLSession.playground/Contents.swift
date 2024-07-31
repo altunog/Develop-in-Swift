3493280abe860cda8ea78ad7e7b91cd1e4bb5b67
Oğuz Kaan Altun <altunog@gmail.com>
2024-07-31 23:00:47 +0300

    Use URLComponents for network request

--- b - Data Collections/4 - WWW HTTP and URL Session/HTTPandURLSession.playground/Contents.swift
+++ b - Data Collections/4 - WWW HTTP and URL Session/HTTPandURLSession.playground/Contents.swift
@@ -14,3 +14,19 @@ Task {
         print(string)
     }
 }
+
+var urlComponents = URLComponents(string: "https://api.nasa.gov/planetary/apod")!
+urlComponents.queryItems = [
+    "api_key": "DEMO_KEY",
+    "date": "2013-07-16"
+].map { URLQueryItem(name: $0.key, value: $0.value) }
+
+Task {
+    let (data, response) = try await URLSession.shared.data(from: urlComponents.url!)
+    
+    if let httpResponse = response as? HTTPURLResponse,
+       httpResponse.statusCode == 200,
+       let string = String(data: data, encoding: .utf8) {
+        print(string)
+    }
+}
